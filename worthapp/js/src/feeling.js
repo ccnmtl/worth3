842bf88b729ed2593e3976b052a4cf2038421d72
Nik Nyby <nnyby@columbia.edu>
2017-06-22 11:58:09 -0400

    feelings check-in station fixes - PMT #110362

--- worthapp/js/src/feeling.js
+++ worthapp/js/src/feeling.js
@@ -11,12 +11,15 @@
 
         $container.on('click', 'figure', function(e) {
             e.preventDefault();
-            var $myContainer = $(this).closest('.container');
-            var feeling = $.trim($(this).attr('alt'));
+            var $this = $(this);
+            var $myContainer = $this.closest('.container');
+            var feeling = $.trim($this.find('img').attr('alt'));
+            var src = $.trim($this.find('img').attr('src'));
 
             $myContainer.find('.my-feeling').text(feeling);
+            $myContainer.find('.feelings-choice img').attr('src', src);
 
-            $(this).closest('div.panel').addClass('hidden');
+            $this.closest('div.panel').addClass('hidden');
             $myContainer.find('div.s1').removeClass('hidden');
             updateProgressBar($myContainer, 100);
         });
