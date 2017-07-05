ae8b08eb8a7ea20364b6e0c81b08f1148bff49af
Susan Dreher <sdreher@columbia.edu>
2017-07-05 13:23:47 -0400

    POP-ping out our problems

---
+++ worthapp/js/src/pop.js
@@ -0,0 +1,22 @@
+/* eslint-env es6 */
+/* global $, initActivityPanels */
+
+(function() {
+    $(document).ready(function() {
+        var $container = $('.container.popping-our-problems');
+        initActivityPanels($container);
+
+        $container.find('button[type="submit"]').click(function(e) {
+            e.preventDefault();
+            var $this = $(this);
+            var $panel = $this.parents('.panel').first();
+            $panel.find('input.show-correct')
+                  .parents('.form-check-label')
+                  .addClass('alert-success');
+
+            $panel.find('.alert').removeClass('hidden');
+            $this.hide();
+            $this.next().show();
+        });
+    });
+})();
