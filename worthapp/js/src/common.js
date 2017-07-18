42bd7bd6adf88e45e9f6eba74cf914d19f127467
Susan Dreher <sdreher@columbia.edu>
2017-07-18 12:46:01 -0400

    Optimize video loading
    Video loads occasionally seem to fail. The video ready state comes through
    as 1 (HAVE_METADATA - metadata for the audio/video is ready). If the video
    state is not 4 when the video comes into view, force a load to ensure things
    are ready to go.

--- worthapp/js/src/common.js
+++ worthapp/js/src/common.js
@@ -1,11 +1,22 @@
 /* global $ */
 /* eslint-env es6 */
 /* exported updateProgressBar, advanceToPanel, initActivityPanels */
-/* exported pauseVideos, isFormComplete, onClickGetAnswers */
+/* exported pauseVideos, readyVideos, isFormComplete, onClickGetAnswers */
 
 /*
  * Stop playback of any playing videos in the given container.
  */
+
+var readyVideos = function($container) {
+    if ($container) {
+        $.each($container.find('video'), function() {
+            if (this.readyState !== 4) {
+                this.load(); // reload
+            }
+        });
+    }
+};
+
 var pauseVideos = function($container) {
     if ($container) {
         $.each($container.find('video'), function() {
@@ -30,6 +41,7 @@ var advanceToPanel = function($container, i, total) {
     $(document).scrollTop(0);
 
     pauseVideos($container);
+    readyVideos($container);
 };
 
 var bindActivityButtons = function($container, idx, total) {
